a649a401ec8954209903672e7ace5d7329fa7f34
Diliana007 <dilianathompson@gmail.com>
2025-03-09 03:29:22 +0000

    Finished

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
 
 class Program
-{
+{ 
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+    {    
+        List<int> numbers = new List<int>(); // Create empty list to store numbers
+        int userNumber = -1; //Initialize with dummy value to enter loop
+
+        while (userNumber !=0) //Loop until user enter 0
+        {
+            Console.Write("Enter a number (0 to quit): " ); //show prompt
+            string userResponse = Console.ReadLine(); //Get user input  as string
+            userNumber = int.Parse(userResponse); //Convert input to integer
+
+            if (userNumber !=0)// only add non Zero numbers
+            {
+                numbers.Add(userNumber); // Add numbers to the list
+            }
+
+            int sum = 0; // Reset sum for each interaction
+            foreach (int number in numbers) // Loop through all numbers
+            {
+                sum += number;   // Acummulate total summ
+            } 
+            Console.WriteLine($"The sum is: {sum}"); // Show current sum
+
+            if (numbers.Count > 0) //Safety check for empty list
+            {
+                int max = numbers[0]; // start with first number as max
+                foreach (int number in numbers)// Check all numbers 
+                {
+                  if  (number > max) // Compare with current max
+                  {
+                       max = number; //Update max if larger number found
+                  }
+                } 
+                Console.WriteLine($"The max is: {max}");// Show current
+            }
+        }  
+    
     }
+
 }
