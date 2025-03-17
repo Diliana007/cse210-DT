5a0e6f5b81bac6d486369c889af9c65115a1d6de
Diliana007 <dilianathompson@gmail.com>
2025-03-17 21:43:16 +0000

    Working in progress

--- week03/Fractions/Fractions.cs
+++ week03/Fractions/Fractions.cs
@@ -52,4 +52,3 @@ public class Fraction
         _bottom = bottom;
     }
 }
-)
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,45 +1,30 @@
 using System;
 
-public class Fraction
+class Program
 {
-    private int _top;
-    private int _bottom;
-
-    public Fraction()
+   static void Main()
     {
               
-        _top = 1;
-        _bottom = 1;
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+        
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        Fraction fraction4 = new Fraction(1, 3);
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
+
+        fraction1.SetTop(2);
+        fraction1.SetBottom(3);
+        Console.WriteLine(fraction1.GetFractionSrting());
+        Console.WriteLine(fraction1.GetDecimalValue());    
     }
 
-    public Fraction (int wholeNumber)
-    {
-        _top  = wholeNumber;
-        _bottom = 1;
-    }
-
-    public Fraction(int top,int bottom)
-    {
-        if(bottom == 0)
-        {
-            throw new ArgumentException("Denominator cannot be zero");
-        }
-        _top = top;
-        _bottom = bottom;
-    }
-    public string GetFractionString()
-    {
-        string text = $"{_top}/{_bottom}";
-        return text;
-    }
-
-    public double GetDecimalValue()
-    {
-        return (double)_top / (double)_bottom;
-    }
-
-    public override string ToString()
-    {
-        return GetFractionString();
-    }
 }
