8b63104fa9b3fdd3d11b22523736fab1cab75363
Diliana007 <dilianathompson@gmail.com>
2025-03-21 17:19:10 +0000

    In Progress

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+       Reference reference = new Reference("Proverbs", 3,5,6);
+       Scripture scripture = new Scripture(reference, "Trust in the Lord with all your heart and lean not on yur own understanding");
+
+       while (!scripture.IsCompletetelyHidden())
+       {
+            Console.WriteLine(scripture.GetDysplayText());
+            Console.WriteLine("\nPress Enter to hide words or type 'quit' to exit:");
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(2); // Hide two random words per turn
+       }
+
+       Console.WriteLine("\nProgram Ended. Final Scripture:");
+       Console.WriteLine(scripture.GetDisplayText());
+
     }
 }
